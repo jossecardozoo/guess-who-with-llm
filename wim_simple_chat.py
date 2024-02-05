84cb0d41a7ad8ac859044051318d36e6491469c4
Anaclara <anaclara@192.168.1.2>
2024-02-05 09:48:06 -0300

    Add candidates in chatbot and start trying to filter them with the bot response

--- wim_simple_chat.py
+++ wim_simple_chat.py
@@ -15,27 +15,43 @@ def configure_chat():
 
     # Algunas respuestas para probar esta parte, la idea esq aca solo se defina cual es el peronaje a adivinar,
     # las respuestas van a ser dadas luego por el modelo entrenado.
-    responses = {
+    chatbot['responses'] = {
         "hello": "Hello! How can I help you?",
-        "goodbye": "Goodbye. Have a great day!",
-        "Is your hair color blue?":"Yes",
-        "Are your eyes brown?": "No"
+        "goodbye": "Goodbye. Have a great day!"
     }
-    chatbot['responses'] = responses
 
     # Ruta a donde estan todos mis characters
     json_file_path = 'all_my_characters.json'
     with open(json_file_path, 'r') as json_file:
         data = json.load(json_file)
 
+    # Seleccionar nueve personajes, de los cuales elegire uno como el candidato
+    random_list_characters = random.sample(list(data.keys()), 9)
+    my_group_of_characters = {key: data[key] for key in random_list_characters if key in data}
+
     # Seleccionar un personaje aleatorio del diccionario
-    random_element = random.choice(list(data.keys()))
-    print(random_element)
-    print(data[random_element])
-    chatbot['my_character'] = random_element
-    chatbot['my_character_attributes'] = data[random_element]
+    random_element_code = random.choice(random_list_characters)
+
+    # Guardar la info de esta partida en `chatbot`
+    chatbot['my_group_of_chars'] = my_group_of_characters
+    chatbot['my_character'] = random_element_code
+    chatbot['my_character_attributes'] = data[random_element_code]
+    del  chatbot['my_character_attributes']['image_file']
+
+    chatbot['my_character_in_one_line'] = character_attributes_in_a_sentence(chatbot['my_character_attributes'])
+    print (chatbot['my_character_in_one_line'])
 
     return chatbot
+
+def character_attributes_in_a_sentence(attributes_in_json):
+    sentence = f"The person has {attributes_in_json['eye_color']} eyes, {attributes_in_json['face_color']} skin, and {attributes_in_json['hair_color']} hair."
+
+    if attributes_in_json['glasses'] == 'yes glasses':
+        sentence += f" They are wearing {attributes_in_json['glasses_color']} glasses."
+    else:
+        sentence += "They aren't wearing glasses."
+
+    return sentence
 
 # preprocesar la pregunta para que me quede todo como un conjunto de palabras -> CAPAZ NO ES NECESARIO HACERLO
 def preprocess_text(question):
@@ -54,14 +70,30 @@ def preprocess_text(question):
 
 # preguntarle al modelo la respuesta con la pregunta ya preprocesada
 def search_response(chatbot, question):
-    processed_question = preprocess_text(question)
-    print(processed_question)
+    # processed_question = preprocess_text(question)
+    # print(processed_question)
 
     # Le pregunto a mi modelo si es correcta o no la pregunta en funcion de mi personaje elegido
-    # Tengo que encontrar la forma de pasar los atributos de mi personaje elegido a una oracion como la que esta abajo.
-    #  "eye_color","face_color", "hair_color", "glasses", "glasses_color", "eyebrow_width"
-    character = "eye color is green, face color is black, hair color is blue, i have glasses, my glasses color is black"
-    return ask_wim_model(question, character)
+    return ask_wim_model(question, chatbot['my_character_in_one_line'])
+
+def find_criteria_by_question_and_aswer(user_question, bot_answer):
+    criteria = {"eye_color": "green"}
+
+    return criteria, True
+
+def filter_candidates(candidates, criteria, include_matching=True):
+    filtered_candidates = {}
+
+    for candidate_id, attributes in candidates.items():
+        meets_criteria = all(attributes.get(attribute, "") == value for attribute, value in criteria.items())
+
+        if include_matching and meets_criteria:
+            filtered_candidates[candidate_id] = attributes
+        elif not include_matching and not meets_criteria:
+            filtered_candidates[candidate_id] = attributes
+
+    return filtered_candidates
+
 
 # Main function
 def run_chat():
@@ -70,14 +102,20 @@ def run_chat():
     chatbot = configure_chat()
 
     while True:
-        user_question = input("User: ")
+        user_question = input("Ask about my character: ")
 
         if user_question.lower() == 'goodbye':
             print("Goodbye. Have a great day!")
             break
 
         bot_response = search_response(chatbot, user_question)
-        print("Chat:", bot_response)
+        # Luego de obtener la respuesta, es aca donde deberia eliminar candidatos
+        # print(chatbot['my_group_of_chars'])
+        # criteria = {"eye_color": "green"} tiene que ser algo asi lo retorne mi funcion
+        criteria, positive_or_negative_matching = find_criteria_by_question_and_aswer(user_question, bot_response)
+        filtered_candidates = filter_candidates(chatbot['my_group_of_chars'], criteria, include_matching=positive_or_negative_matching)
+        # print(filtered_candidates)
+        print(bot_response)
 
 if __name__ == "__main__":
     run_chat()
