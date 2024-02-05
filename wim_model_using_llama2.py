92f4bae33ff9306c94cb5843734a296c130a7f48
Anaclara <anaclara@192.168.1.2>
2024-02-05 09:45:53 -0300

    Enhacement of the system prmt of llama2 model

--- wim_model_using_llama2.py
+++ wim_model_using_llama2.py
@@ -8,6 +8,7 @@ def ask_wim_model(question, character):
 def answering(question, character):
 # The meta/llama-2-70b-chat model can stream output as it's running.
     model_version = "meta/llama-2-70b-chat"
+    answer = ""
 
     for event in replicate.stream(
         model_version,
@@ -17,12 +18,17 @@ def answering(question, character):
             "top_p": 1,
             "prompt": f"Pleas answer this question about the character: {question}",
             "temperature": 0.5,
-            "system_prompt": f"You are a helpful, respectful and honest assistant. You have this phisical attributes: {character}. You have to answer the question without reveling the correct value of the attributes",
-            "max_new_tokens": 10,
+            "system_prompt": f"You are responding to a kid, so be kind and generate short answers. You have this phisical attributes: {character}. You have to answer the question without reveling the correct answer and including a yes or a no",
+            "max_new_tokens": 25,
             "min_new_tokens": -1
         },
     ):
-        print(str(event), end="")
+        # print(str(event), end="")
+        answer = answer + str(event) + ""
+
+    return answer
 
 # character = "eye color is green, face color is black, hair color is blue, i have glasses, my glasses color is black"
 # answering("Are your eyes green?",character)
+
+# "system_prompt": "You are responding to a kid, so be kind and use simple lenguaje. You have this phisical attributes: {character}. You have to answer the question with yes or no",
