4df1b193b8645462ce1c81c6425e7c798d054cb8
Anaclara <anaclara@Anaclaras-MacBook-Pro.local>
2024-06-27 17:14:18 -0300

    Clean wim_simple_chat.py file

--- wim_simple_chat.py
+++ wim_simple_chat.py
@@ -10,16 +10,9 @@ from wim_model_using_llama2 import ask_wim_model
 # from wim_llama2_model_locally import ask_wim_model
 
 
-# Configurar el chat
+# Configurar el arranque del juego
 def configure_chat():
     chatbot = {}
-
-    # Algunas respuestas para probar esta parte, la idea esq aca solo se defina cual es el peronaje a adivinar,
-    # las respuestas van a ser dadas luego por el modelo entrenado.
-    chatbot['responses'] = {
-        "hello": "Hello! How can I help you?",
-        "goodbye": "Goodbye. Have a great day!"
-    }
 
     # Ruta a donde estan todos mis characters
     json_file_path = 'all_my_characters.json'
@@ -86,15 +79,11 @@ def character_attributes_in_a_sentence(attributes_in_json):
 
     return sentence
 
-# preguntarle al modelo la respuesta con la pregunta
 def search_response(chatbot, question):
-    # Le pregunto a mi modelo si es correcta o no la pregunta en funcion de mi personaje elegido
     return ask_wim_model(question, chatbot['my_character_in_one_line'])
 
 def eliminate_candidate(chatbot, image_path):
     candidate_to_eliminate = os.path.basename(image_path).split('.')[0]
-    # print(candidate_to_eliminate)
-    # print(chatbot['my_group_of_chars'])
     chatbot['my_group_of_chars'].pop(candidate_to_eliminate)
 
     print("sin el elemento a eliminar")
@@ -105,46 +94,3 @@ def eliminate_candidate(chatbot, image_path):
         status = "Game over"
 
     return chatbot, status
-
-# def find_criteria_by_question_and_aswer(user_question, bot_answer):
-#     criteria = {"eye_color": "green"}
-
-#     return criteria, True
-
-# def filter_candidates(candidates, criteria, include_matching=True):
-#     filtered_candidates = {}
-
-#     for candidate_id, attributes in candidates.items():
-#         meets_criteria = all(attributes.get(attribute, "") == value for attribute, value in criteria.items())
-
-#         if include_matching and meets_criteria:
-#             filtered_candidates[candidate_id] = attributes
-#         elif not include_matching and not meets_criteria:
-#             filtered_candidates[candidate_id] = attributes
-
-#     return filtered_candidates
-
-# Main function
-# def run_chat():
-#     print("Hello! Let's start playing. Type 'goodbye' to exit.")
-    
-#     chatbot = configure_chat()
-
-#     while True:
-#         user_question = input("Ask about my character: ")
-
-#         if user_question.lower() == 'goodbye':
-#             print("Goodbye. Have a great day!")
-#             break
-
-#         bot_response = search_response(chatbot, user_question)
-#         # Luego de obtener la respuesta, es aca donde deberia eliminar candidatos
-#         # print(chatbot['my_group_of_chars'])
-#         # criteria = {"eye_color": "green"} tiene que ser algo asi lo retorne mi funcion
-#         criteria, positive_or_negative_matching = find_criteria_by_question_and_aswer(user_question, bot_response)
-#         filtered_candidates = filter_candidates(chatbot['my_group_of_chars'], criteria, include_matching=positive_or_negative_matching)
-#         # print(filtered_candidates)
-#         print(bot_response)
-
-# if __name__ == "__main__":
-#     run_chat()
