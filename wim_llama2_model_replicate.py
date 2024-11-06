43a275cc87c0c766dca15899224142a5867c0c77
josefina.cardozo <josefina.cardozo@fing.edu.uy>
2024-11-06 08:00:22 -0300

    Adaptacion para usar groq

--- wim_llama2_model_replicate.py
+++ wim_llama2_model_replicate.py
@@ -1,30 +1,31 @@
-import replicate
+import os
 
-########################### LLAMA2 WITH REPLICATE #############################
+from groq import Groq
+
+client = Groq(api_key="")
+
+
 def ask_wim_model(question, character):
     answer = answering(question, character)
     return answer
 
+
 def answering(question, character):
-    model_version = "meta/llama-2-70b-chat"
-    answer = ""
+    prompt = (f"You are responding to a kid, so be kind and generate short answers. "
+              f"You have these physical attributes: {character}. "
+              f"You have to answer the question only with a yes or a no. "
+              f"Question: {question}")
 
-    for event in replicate.stream(
-        model_version,
-        input={
-            "debug": False,
-            "top_k": 50,
-            "top_p": 1,
-            "prompt": f"Pleas answer this question about the character: {question}",
-            "temperature": 0.001,
-            "system_prompt": f"You are responding to a kid, so be kind and generate short answers. You have this phisical attributes: {character}. You have to answer the question only with a yes or a no",
-            "max_new_tokens": 25,
-            "min_new_tokens": -1
-        },
-    ):
-        answer = answer + str(event) + ""
-
-    # Por alguna razon el modelo retorna parte de la respuesta anterior.
-    # Esto es para solo obtener la ultima respuesta, habria que mejorarlo
-    answer_words = answer.split()
-    return answer_words
+    chat_completion = client.chat.completions.create(
+    messages=[
+        {
+            "role": "user",
+            "content": prompt,
+        }
+    ],
+    model="llama3-8b-8192",
+    )
+    answer = chat_completion.choices[0].message.content
+    if (answer == 'undefined'):
+        return "I dont understand"
+    return answer
